b5ca6ad24a7ef31261ac5fbcfb72422a7e7e9817
BLXNK-333 <moto-j@outlook.com>
2024-08-17 23:01:27 +0400

    Изменил немного логику постобработки, удалил лишние тесты.

--- src/post_processing/processing_functions.py
+++ src/post_processing/processing_functions.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Set
 from src.event_handlers.events import AnyEvent
 
 
@@ -43,7 +43,7 @@ def remove_unpaired_up_events(events: List[AnyEvent]):
         else:
             if len(event) >= 4 and isinstance(event[3], (str, int)):
                 button = event[3]
-                _key = "click" + str(button)
+                _key = "click_" + str(button)
                 if _type == "click_down":
                     cleaned_events.append(event)
                     pressed.add(_key)
@@ -51,43 +51,39 @@ def remove_unpaired_up_events(events: List[AnyEvent]):
                     cleaned_events.append(event)
                     pressed.remove(_key)
 
-    return cleaned_events
+    return cleaned_events, pressed
 
 
-def remove_unpaired_down_events(events: List[AnyEvent]):
+def remove_unpaired_down_events(events: List[AnyEvent], pressed: Set[str]):
+    if not pressed:
+        return events
+
     cleaned_events = []
     untracked = {"move", "scroll"}
-    unpressed = set()
 
-    for i in range(len(events) - 1, -1, -1):
-        event = events[i]
-        _type = events[i][0]
+    for event in reversed(events):
+        _type = event[0]
+
         if _type in untracked:
             cleaned_events.append(event)
             continue
 
         if _type.startswith("key"):
             button = event[1]
-            if _type == "key_release":
+            if _type == "key_press" and button in pressed:
+                pressed.remove(button)
+            else:
                 cleaned_events.append(event)
-                unpressed.add(button)
-            elif _type == "key_press" and button in unpressed:
-                cleaned_events.append(event)
-                unpressed.remove(button)
         else:
             if len(event) >= 4 and isinstance(event[3], (str, int)):
                 button = event[3]
-                _key = "click" + str(button)
-                if _type == "click_up":
-                    cleaned_events.append(event)
-                    unpressed.add(_key)
+                _key = f"click_{button}"
+                if _type == "click_down" and _key in pressed:
+                    pressed.remove(_key)
                 else:
-                    if _key in unpressed:
-                        cleaned_events.append(event)
-                        unpressed.remove(_key)
+                    cleaned_events.append(event)
 
-    cleaned_events.reverse()
-    return cleaned_events
+    return cleaned_events[::-1]
 
 
 def cleanup_unpaired_events(events: List[AnyEvent]):
@@ -96,78 +92,7 @@ def cleanup_unpaired_events(events: List[AnyEvent]):
     которые не имеют пары. Например, если клавиша была нажата, но не отжата до
     конца макроса, или наоборот.
     """
-    return remove_unpaired_down_events(
-        remove_unpaired_up_events(events)
-    )
-
-
-def stack_time_keyboard(events: List[AnyEvent]):
-    """
-    Складывает время задержек для последовательных нажатий или отпусканий
-    одной и той же клавиши.
-    """
-    if not events:
-        return []
-
-    stacked_events = []
-
-    empty_field = ("", "", 0.0)
-    events.append(empty_field)
-    prev_event, prev_key, stacked_delay = empty_field
-
-    for i in range(len(events)):
-        event_type = events[i][0]
-        if event_type.startswith("key"):
-            cur_event, cur_key, delay = events[i][:3]
-            if cur_key == prev_key and cur_event == prev_event:
-                stacked_delay += delay
-                continue
-
-            if prev_key:
-                round_delay = round(stacked_delay, 10)
-                stacked_events.append((prev_event, prev_key, round_delay))
-            prev_event, prev_key, stacked_delay = cur_event, cur_key, delay
-
-        else:
-            if prev_key:
-                round_delay = round(stacked_delay, 10)
-                stacked_events.append((prev_event, prev_key, round_delay))
-            stacked_events.append(events[i])
-            prev_event, prev_key, stacked_delay = empty_field
-    if stacked_events[-1] == empty_field:
-        stacked_events.pop()
-    return stacked_events
-
-
-def stack_time_mouse(events: List[AnyEvent]):
-    if not events:
-        return []
-
-    stacked_events = []
-    empty_field = ("", 0, 0, 0.0)
-    events.append(empty_field)
-    prev_event, prev_x, prev_y, stacked_delay = empty_field
-
-    for i in range(len(events)):
-        event_type = events[i][0]
-
-        if event_type == "move":
-            cur_event, cur_x, cur_y, delay = events[i][:4]
-            if isinstance(delay, float) and isinstance(stacked_delay, float):
-                if cur_x == prev_x and cur_y == prev_y:
-                    stacked_delay += delay
-                    continue
-            if prev_event:
-                stacked_events.append((prev_event, prev_x, prev_y, stacked_delay))
-            prev_event, prev_x, prev_y, stacked_delay = cur_event, cur_x, cur_y, delay
-        else:
-            if prev_event:
-                stacked_events.append((prev_event, prev_x, prev_y, stacked_delay))
-            stacked_events.append(events[i])
-            prev_event, prev_x, prev_y, stacked_delay = empty_field
-    if stacked_events[-1] == empty_field:
-        stacked_events.pop()
-    return stacked_events
+    return remove_unpaired_down_events(*remove_unpaired_up_events(events))
 
 
 def insert_hot_corner_activate(events: List[AnyEvent]):
@@ -219,14 +144,8 @@ def post_process_input_events(events: List[AnyEvent]) -> List[AnyEvent]:
     :param events: Записанный макрос.
     :return: Обработанный макрос.
     """
-    # return insert_hot_corner_activate(
-    #     cleanup_unpaired_events(
-    #         stack_time_mouse(
-    #             stack_time_keyboard(
-    #                 convert_time_to_delays(events)
-    #             )
-    #         )
-    #
-    #     )
-    # )
-    return insert_hot_corner_activate(convert_time_to_delays(events))
+    return insert_hot_corner_activate(
+        cleanup_unpaired_events(
+                convert_time_to_delays(events)
+                )
+            )
--- test/test_processing_functions/test_remove_unpaired_down_events.py
+++ test/test_processing_functions/test_remove_unpaired_down_events.py
@@ -2,85 +2,100 @@ import unittest
 from src.post_processing.processing_functions import remove_unpaired_down_events
 
 
-class TestRemoveUnpairedDownEvents(unittest.TestCase):
-    def test_remove_key_events(self):
+class TestRemoveUnpairedDownEventsWithPressedSet(unittest.TestCase):
+    def test_no_unpaired_down_events(self):
         events = [
             ("key_press", "a", 0.1),
             ("key_release", "a", 0.2),
             ("key_press", "b", 0.3),
-            ("key_release", "b", 0.5),
-            ("key_press", "c", 0.4),  # Непарное событие
+            ("key_release", "b", 0.4),
         ]
+        pressed = set()
+        expected = events
+        self.assertEqual(remove_unpaired_down_events(events, pressed), expected)
+
+    def test_remove_unpaired_down_key_event(self):
+        events = [
+            ("key_press", "a", 0.1),
+            ("key_press", "b", 0.2),
+            ("key_release", "b", 0.3),
+            ("key_press", "a", 0.4),
+        ]
+        pressed = {"a"}
         expected = [
+            ("key_press", "a", 0.1),
+            ("key_press", "b", 0.2),
+            ("key_release", "b", 0.3),
+        ]
+        self.assertEqual(remove_unpaired_down_events(events, pressed), expected)
+
+    def test_remove_unpaired_down_mouse_event(self):
+        events = [
+            ("click_down", 100, 200, "left", 0.1),
+            ("click_down", 100, 200, "right", 0.2),
+            ("click_up", 100, 200, "right", 0.3),
+            ("click_down", 100, 200, "left", 0.4),
+        ]
+        pressed = {"click_left"}
+        expected = [
+            ("click_down", 100, 200, "left", 0.1),
+            ("click_down", 100, 200, "right", 0.2),
+            ("click_up", 100, 200, "right", 0.3),
+        ]
+        self.assertEqual(remove_unpaired_down_events(events, pressed), expected)
+
+    def test_intermixed_events_with_untracked(self):
+        events = [
+            ("move", 100, 200, 0.05),
+            ("key_press", "a", 0.1),
+            ("scroll", 627, 271, 0, -1, 0.15),
+            ("key_press", "b", 0.2),
+            ("key_release", "b", 0.3),
+        ]
+        pressed = {"a"}
+        expected = [
+            ("move", 100, 200, 0.05),
+            ("scroll", 627, 271, 0, -1, 0.15),
+            ("key_press", "b", 0.2),
+            ("key_release", "b", 0.3),
+        ]
+        self.assertEqual(remove_unpaired_down_events(events, pressed), expected)
+
+    def test_no_pressed_set(self):
+        events = [
             ("key_press", "a", 0.1),
             ("key_release", "a", 0.2),
             ("key_press", "b", 0.3),
-            ("key_release", "b", 0.5),
+            ("key_release", "b", 0.4),
         ]
-        self.assertEqual(remove_unpaired_down_events(events), expected)
+        pressed = set()
+        expected = events
+        self.assertEqual(remove_unpaired_down_events(events, pressed), expected)
 
-    def test_remove_mouse_events(self):
+    def test_all_unpaired_down_events(self):
         events = [
-            ("click_down", 100, 200, "left", 0.1),
-            ("click_up", 100, 200, "left", 0.2),
-            ("click_down", 100, 200, "right", 0.3),
-            ("click_down", 100, 200, "middle", 0.4),  # Непарное событие
-            ("click_up", 100, 200, "right", 0.5),
+            ("key_press", "a", 0.1),
+            ("key_press", "b", 0.2),
+            ("click_down", 100, 200, "left", 0.3),
         ]
-        expected = [
-            ("click_down", 100, 200, "left", 0.1),
-            ("click_up", 100, 200, "left", 0.2),
-            ("click_down", 100, 200, "right", 0.3),
-            ("click_up", 100, 200, "right", 0.5),
-        ]
-        self.assertEqual(remove_unpaired_down_events(events), expected)
-
-    def test_move_and_scroll_events(self):
-        events = [
-            ("move", 100, 200, 0.1),
-            ("scroll", 627, 271, 0, -1, 0.05),
-            ("key_press", "a", 0.3),
-            ("key_release", "a", 0.4),
-        ]
-        expected = [
-            ("move", 100, 200, 0.1),
-            ("scroll", 627, 271, 0, -1, 0.05),
-            ("key_press", "a", 0.3),
-            ("key_release", "a", 0.4),
-        ]
-        self.assertEqual(remove_unpaired_down_events(events), expected)
-
-    def test_empty_events(self):
-        events = []
+        pressed = {"a", "b", "click_left"}
         expected = []
-        self.assertEqual(remove_unpaired_down_events(events), expected)
-
-    def test_all_unpaired_events(self):
-        events = [
-            ("key_press", "Key.ctrl", 0.02),
-            ("key_press", "Key.space", 1.05),
-            ("click_down", 100, 200, "left", 0.2),
-            ("key_press", "a", 0.03),
-        ]
-        expected = []
-        self.assertEqual(remove_unpaired_down_events(events), expected)
+        self.assertEqual(remove_unpaired_down_events(events, pressed), expected)
 
     def test_shaffle_events(self):
         events = [
-            ("key_press", "Key.ctrl", 0.02),
-            ("click_down", 100, 200, "right", 0.3),
-            ("click_down", 100, 250, "right", 0.2),
-            ("scroll", 627, 271, 0, -1, 0.05),
-            ("key_press", "a", 0.3),
-            ("key_release", "Key.ctrl", 0.02),
-            ("key_press", "Key.cmd", 1.02)
+            ("move", 603, 455, 0.0077),
+            ("click_down", 603, 455, "left", 0.08827),
+            ("click_up", 603, 455, "left", 0.4709),
+            ("key_press", "Key.ctrl", 0.1338)
         ]
+        pressed = {"Key.ctrl"}
         expected = [
-            ("key_press", "Key.ctrl", 0.02),
-            ("scroll", 627, 271, 0, -1, 0.05),
-            ("key_release", "Key.ctrl", 0.02)
+            ("move", 603, 455, 0.0077),
+            ("click_down", 603, 455, "left", 0.08827),
+            ("click_up", 603, 455, "left", 0.4709)
         ]
-        self.assertEqual(remove_unpaired_down_events(events), expected)
+        self.assertEqual(remove_unpaired_down_events(events, pressed), expected)
 
 
 if __name__ == '__main__':
