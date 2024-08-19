429bbbeb833f0f88f06ea77a029ee3670bf3aef8
BLXNK-333 <moto-j@outlook.com>
2024-08-20 03:57:53 +0400

    Рефакторинг run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# Путь до интерпретатора и скрипта
-VENV_PATH="/home/blxnk/PycharmProjects/click_key_capture/.venv"
+# Определяем путь до директории скрипта
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-SCRIPT_DIR="/home/blxnk/PycharmProjects/click_key_capture"
+# Путь до интерпретатора и скрипта
+VENV_PATH="$SCRIPT_DIR/.venv"
 PY_SCRIPT_PATH="$SCRIPT_DIR/main.py"
+
+# Переходим в директорию скрипта
 cd "$SCRIPT_DIR" || exit
 
-source $VENV_PATH/bin/activate
+# Активация виртуального окружения
+source "$VENV_PATH/bin/activate"
+
+# Запуск Python-скрипта
 python3 "$PY_SCRIPT_PATH" "$@"
 
+# Деактивация виртуального окружения
 deactivate
